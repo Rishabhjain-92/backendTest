c39608534ffe8d93bb2824cbacbb8ded1b08d5a1
Rishabhjain-92 <rishabhjain92148@gmail.com>
2025-12-18 17:58:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/twitter',(req,res)=>{
+    res.send('rishabh123')
+
+})
+app.get('/login',(req,res) =>{
+    res.send('<h1>please login at chai aur code</h1>')
+
+})
+app.get('/youtube',(req,res)=>{
+    res.send('<h2>chai aur code</h2>')
+
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
