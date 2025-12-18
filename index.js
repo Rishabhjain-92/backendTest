e51e6c8c89233c4aa559c0f577b3afcbeaf44c7e
Rishabhjain-92 <rishabhjain92148@gmail.com>
2025-12-18 18:17:55 +0530

    github add data

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express')
 require('dotenv').config()
 const app = express()
 const port = 4000
+const githubData ={
+    "location":"India",
+    
+
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -18,7 +23,10 @@ app.get('/youtube',(req,res)=>{
     res.send('<h2>chai aur code</h2>')
 
 })
+app.get('/github',(req,res)=>{
+    res.json(githubData)
 
+})
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${process.env.PORT}`)
 })
